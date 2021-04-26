e63b3871a36a3ba9d9f5a1ecddb4142c84d7adaf
Oscar Ortiz <oscarleonardoortizramirez@gmail.com>
2021-04-26 17:44:44 -0500

    vistas

--- lib/src/view/inicioSesionTelefono.vista.dart
+++ lib/src/view/inicioSesionTelefono.vista.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:bouncing_widget/bouncing_widget.dart';
+import 'package:taxis/src/view/codigoVerificacionInicioSesion.vista.dart';
 
 class InicioSesionTelefono extends StatefulWidget {
   InicioSesionTelefono({Key key}) : super(key: key);
@@ -16,56 +17,77 @@ class _InicioSesionTelefonoState extends State<InicioSesionTelefono> {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.only(top: height / 9),
-            child: Image(
-              image: AssetImage('assets/logo-texto.png'),
-              width: 150,
-            ),
-          ),
-          Text(
-            "Inicia con tu telefono",
-            style: TextStyle(fontSize: 25),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: height / 9, bottom: height / 9),
-            child: Image(
-              image: AssetImage('assets/logo-texto.png'),
-              width: 150,
-            ),
-          ),
-          Container(
-              child: Center(
-            child: BouncingWidget(
-              duration: Duration(milliseconds: 100),
-              scaleFactor: 1.5,
-              onPressed: () {},
-              child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50.0),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: height / 7),
+                child: Center(
+                  child: Image(
+                    image: AssetImage('assets/logo-texto.png'),
+                    height: height / 3,
                   ),
-                  color: Color.fromRGBO(253, 213, 1, 1),
-                  child: Container(
-                    width: width / 3,
-                    height: height / 20,
-                    child: Center(
-                      child: Text(
-                        "Listo",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: height / 60),
+                ),
+              ),
+              Container(
+                  margin: EdgeInsets.only(top: height / 15),
+                  width: width / 1.2,
+                  child: Center(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Número de teléfono',
+                        hintStyle: TextStyle(
+                          color: Colors.black,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                        ),
+                        fillColor: Color.fromRGBO(253, 213, 1, 1),
+                        hoverColor: Colors.grey,
+                        focusColor: Colors.grey,
                       ),
+                      onTap: () {},
+                      onChanged: (text) {},
+                      onSubmitted: (data) {},
                     ),
                   )),
-            ),
-          )),
-        ],
-      ),
-    );
+              Container(
+                  margin: EdgeInsets.only(top: height / 20),
+                  child: Center(
+                    child: BouncingWidget(
+                      duration: Duration(milliseconds: 100),
+                      scaleFactor: 1.5,
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    CodigoVerificacionInicioSesion()));
+                      },
+                      child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50.0),
+                          ),
+                          color: Color.fromRGBO(253, 213, 1, 1),
+                          child: Container(
+                            width: width / 3,
+                            height: height / 20,
+                            child: Center(
+                              child: Text(
+                                "Verificar",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: height / 60),
+                              ),
+                            ),
+                          )),
+                    ),
+                  )),
+            ],
+          ),
+        ));
   }
 }
